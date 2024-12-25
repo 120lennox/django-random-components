41aae8781cde0640dbf2dbaec5f03626214ee3e8
Lennox Mountain <lennoxmountain67@gmail.com>
2024-12-25 19:46:04 +0200

    init installations

--- crudstart/settings.py
+++ crudstart/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'dj_rest_auth',
+
+    # local apps
+    'users',
 ]
 
 MIDDLEWARE = [
