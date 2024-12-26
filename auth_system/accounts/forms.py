120a6cffda86be174d58a89333713dd02c18dfab
Lennox Mountain <lennoxmountain67@gmail.com>
2024-12-26 11:20:23 +0200

    added user forms

---
+++ auth_system/accounts/forms.py
@@ -0,0 +1,14 @@
+from .models import CustomUser
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+
+# user creation form
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta(UserCreationForm):
+        model = CustomUser
+        fields = UserCreationForm.Meta.fields + ("name",)
+
+class customUserchangeForm(UserChangeForm):
+    class Meta(UserChangeForm):
+        model = CustomUser
+        fields = UserChangeForm.Meta.fields
