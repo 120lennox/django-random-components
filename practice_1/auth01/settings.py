6c3ef55bafd4e352b1707fb00a87bf3126d46c2d
Lennox Mountain <lennoxmountain67@gmail.com>
2025-01-10 16:17:56 +0200

    migrations

--- practice_1/auth01/settings.py
+++ practice_1/auth01/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    #local apps
+    'blog',
+
+    #3rd party apps
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
